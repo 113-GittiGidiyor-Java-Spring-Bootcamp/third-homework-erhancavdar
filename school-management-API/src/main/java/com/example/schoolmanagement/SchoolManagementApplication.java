810bfc4118aa69d358aa929789bace109017ea0f
rhancav <edcavdar@gmail.com>
2021-08-24 02:12:17 +0300

    currency converter

--- school-management-API/src/main/java/com/example/schoolmanagement/SchoolManagementApplication.java
+++ school-management-API/src/main/java/com/example/schoolmanagement/SchoolManagementApplication.java
@@ -2,6 +2,9 @@ package com.example.schoolmanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
@@ -10,6 +13,11 @@ public class SchoolManagementApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SchoolManagementApplication.class, args);
+    }
+
+    @Bean
+    public RestTemplate buildRestTemplate(RestTemplateBuilder restTemplateBuilder){
+        return restTemplateBuilder.build();
     }
 
 }
--- school-management-API/src/main/java/com/example/schoolmanagement/entity/GenderStatistics.java
+++ school-management-API/src/main/java/com/example/schoolmanagement/entity/GenderStatistics.java
@@ -1,6 +1,6 @@
 package com.example.schoolmanagement.entity;
 
-
+// Projection interface
 public interface GenderStatistics {
     Gender getGender();
 
